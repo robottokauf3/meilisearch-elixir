0b3f91be46bd78213c3096601515b26f1573b13e
Rob Kaufmann <rok3@rok3.me>
2021-09-02 20:06:59 -0400

    Update default testing endpoint to latest version in docker-compose.yml

--- config/test.exs
+++ config/test.exs
@@ -1,6 +1,7 @@
 use Mix.Config
 
 config :meilisearch,
+  # Latest version from docker-compose.yml
   endpoint: "http://localhost:7700",
   api_key: "test_api_key",
   test_index: "meilisearch_test_index"
