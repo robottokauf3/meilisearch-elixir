eb69d20211f5b44067c87b114e0627ffd11714c0
Nikolai Kozak <nikokozak@gmail.com>
2022-08-26 17:46:15 -0400

    update authorization method

--- lib/meilisearch/http.ex
+++ lib/meilisearch/http.ex
@@ -89,6 +89,7 @@ defmodule Meilisearch.HTTP do
   defp add_auth_header(headers) do
     api_key = Meilisearch.Config.api_key()
 
-    [{"X-Meili-API-Key", api_key} | headers]
+    # < v0.24, use: [{"X-Meili-API-Key", api_key} | headers]
+    [{"Authorization", "Bearer #{api_key}"} | headers]
   end
 end
